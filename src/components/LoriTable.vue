5d3ddb57be69b3390ab5d2d75e895d9e0f542c0c
shabai517 <shabai517@gmail.com>
2024-06-25 09:14:46 +0100

    change table background color

--- src/components/LoriTable.vue
+++ src/components/LoriTable.vue
@@ -1,10 +1,10 @@
 <template>
-    <div style="background-color: gray;padding: 2px; overflow-x: hidden; width: 100%;">
+    <div style="padding: 2px; width: 100%;">
         <!-- <div>lori table</div>
         <div>{{ loriAttr.title }}</div>
         <Button type="success" @click="submitForm">Submit Table</Button> -->
         <!-- {{ columns }} -->
-        <Table :columns="columns" :data="data" size="small" border></Table>
+        <Table ref="tableView" :columns="columns" :data="data" width="100%" size="small" border></Table>
     </div>
 </template>
 
@@ -14,12 +14,7 @@ export default {
     inject: ["loriAttr"],
     data() {
         return {
-            columns1: [
-                {
-                    title: 'Name',
-                    key: 'name'
-                },
-            ],
+            columns1: [],
             data: [
 
             ]
@@ -41,25 +36,29 @@ export default {
                             title: j == 0 ? groupName + '+' : groupName + (j + 1) + '+',
                             key: groupName + (j + 1),
                             align: 'center',
-                            width: 80
+                            width: 80,
+
                         })
                     }
                 }
 
                 if (groupName == 'c') {
                     tableColumns.push({
+                        className: 'gray-column',
                         title: '#',
                         key: 'seq1',
                         align: 'center',
                         width: 50
                     });
                     tableColumns.push({
+                        className: 'blue-column',
                         title: 'Seq',
                         key: 'seq',
                         align: 'center',
                         width: 70
                     });
                     tableColumns.push({
+                        className: 'gray-column',
                         title: '#',
                         key: 'seq2',
                         align: 'center',
@@ -68,6 +67,7 @@ export default {
                 }
 
             }
+
             return tableColumns;
         }
     },
@@ -75,6 +75,16 @@ export default {
         loriAttr: {
             handler(newValue, oldValue) {
                 console.log("loriAttr changed in table:", newValue, oldValue);
+                const overflowElements = document.querySelectorAll('.ivu-table-overflowX');
+                let element = overflowElements[0];
+                console.log('overflowElements', overflowElements);
+                console.log("element:", element);
+                if (element) {
+                    // element.classList.add('overflowX');
+                    // element.style.overflowX = 'scroll !important';
+                }
+
+
             },
             deep: true,
         }
@@ -92,6 +102,13 @@ export default {
                 line[`c${j}`] = '' + (i + 1);
                 line[`x${j}`] = '' + (i + 1);
                 line[`y${j}`] = '' + (i + 1);
+                if (i >= 2 && i <= 4) {
+                    line['cellClassName'] = { 'a1': 'info-cell' }
+                }
+
+                if (i >= 6 && i <= 8) {
+                    line['cellClassName'] = { 'a2': 'warn-cell' }
+                }
             }
             this.data.push(line);
         }
@@ -99,8 +116,8 @@ export default {
         for (let i = 0; i < this.loriAttr.ions.length; i++) {
             let groupName = this.loriAttr.ions[i].name;
             let groupData = this.loriAttr.ions[i].data;
-            // console.log("groupName:", groupName);
-            // console.log("groupData:", groupData);
+            console.log("groupName:", groupName);
+            console.log("groupData:", groupData);
             for (let j = 0; j < groupData.length; j++) {
                 let checked = groupData[j];
                 if (checked) {
@@ -109,7 +126,8 @@ export default {
                         title: j == 0 ? groupName + '+' : groupName + (j + 1) + '+',
                         key: groupName + (j + 1),
                         align: 'center',
-                        width: 80
+                        width: 80,
+
                     })
                 }
             }
@@ -126,6 +144,45 @@ export default {
 };
 </script>
 
+<style>
+.table table {
+    width: 50% !important;
+}
+
+.ivu-table td.blue-column {
+    background-color: #88baec;
+    /* color: white; */
+}
+
+.ivu-table td.gray-column {
+    background-color: #e8eaec;
+    /* color: white; */
+}
+
+.ivu-table .info-cell {
+    background-color: #65a8f0;
+    color: #fff;
+}
+
+.ivu-table .warn-cell {
+    background-color: #e9c082;
+    color: #fff;
+}
+
+.ivu-table .demo-table-info-cell-name {
+    background-color: #5dbeeb;
+    color: #fff;
+}
+
+.ivu-table-overflowX {
+    overflow-x: hidden;
+}
+
+.overflowX {
+    overflow-x: scrole !important;
+}
+</style>
+
 <style scoped>
 :deep(table) {
     margin-bottom: 0 !important;
@@ -135,4 +192,8 @@ export default {
     padding-left: 0;
     padding-right: 0;
 }
+
+/* :deep(.ivu-table-overflowX) {
+    overflow-x: hidden;
+} */
 </style>
