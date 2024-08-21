1a8c1c6680ced9c21e4faf336f2f740a1e0251ab
shabai517 <shabai517@gmail.com>
2024-08-21 09:50:24 +0100

    add zigzag line demo

--- src/views/ColourfulText.vue
+++ src/views/ColourfulText.vue
@@ -13,6 +13,29 @@
             <path class="line" :d="`M${i*20+ 10},25 L${i*20+ 10},13.5 L${i*20 + 6},6`" opacity="1" style="stroke: rgb(13, 117, 188)"></path>
           </template> -->
 
+          <path class="line" :d="`M${0 * 20 + 10},25 L${0 * 20 + 10},13 L${0 * 20 + 6},6`" opacity="1"
+            style="stroke: rgb(13, 117, 188)">
+          </path>
+
+          <path class="line" :d="`M${1 * 20 + 10},25 L${1 * 20 + 10},37 L${1 * 20 + 14},43`" opacity="1"
+            style="stroke: rgb(13, 117, 188)"></path>
+
+          <path class="line" :d="`M${2 * 20 + 10},25 L${2 * 20 + 10},13 L${2 * 20 + 6},6`" opacity="1"
+            style="stroke: rgb(13, 117, 188)">
+          </path>
+          <path class="line" :d="`M${2 * 20 + 10},25 L${2 * 20 + 10},37 L${2 * 20 + 14},43`" opacity="1"
+            style="stroke: rgb(13, 117, 188)"></path>
+
+          <path class="line" :d="`M${3 * 20 + 10},25 L${3 * 20 + 10},13 L${3 * 20 + 5},6`" opacity="1"
+            style="stroke: rgb(13, 117, 188)">
+          </path>
+          <path class="line" :d="`M${3 * 20 + 10},18 L${3 * 20 + 5},11`" opacity="1" style="stroke: rgb(130, 12, 173)">
+          </path>
+
+          <path class="line" :d="`M${4 * 20 + 10},25 L${4 * 20 + 10},37 L${4 * 20 + 14},43`" opacity="1"
+            style="stroke: rgb(13, 117, 188)"></path>
+          <path class="line" :d="`M${4 * 20 + 10},33 L${4 * 20 + 14},39`" opacity="1" style="stroke: rgb(130, 12, 173)">
+          </path>
         </g>
       </svg>
     </div>
