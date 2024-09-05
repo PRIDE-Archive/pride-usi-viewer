57f80852aa49fa2ed295efcc8026a684a00af672
shabai517 <shabai517@gmail.com>
2024-09-05 15:46:34 +0100

    remove test files

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,4 @@
 import { get, post } from "./request";
-import receive from '@/store/receive.json'
 export const getSpectrum = (usi: string) => {
   return get("/pride/molecules/ws/spectrum", { usi, resultType: 'FULL' });
 };
