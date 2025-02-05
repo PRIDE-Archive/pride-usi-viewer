67e0d28477daee74111eecbabd9071eb7af78d77
shabai517 <shabai517@gmail.com>
2025-02-05 11:21:18 +0000

    update api for annotateUSI and prediction

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,3 +6,11 @@ export const getSpectrum = (usi: string) => {
 export const spectrumAnnotation = (param: any) => {
   return post("/pride/molecules/ws/spectrum_annotations", param);
 };
+
+export const annotateUsi = (param: any) => {
+  return post("/pride/molecules/ws/annotate_usi", param);
+};
+
+export const spectrumPrediction = (usi: string) => {
+  return get("/pride/molecules/ws/spectrum_prediction", { usi, resultType: 'FULL' });
+};
