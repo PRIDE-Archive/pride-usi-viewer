88e0bd725b89dd60e77a3ba1dc5f35b54124021d
shabai517 <shabai517@gmail.com>
2025-02-09 15:07:48 +0000

    change the style

--- src/views/LoriForm1.vue
+++ src/views/LoriForm1.vue
@@ -1,19 +1,22 @@
 <template>
-  <div style="display: flex; justify-content: space-between; align-items: center;border: 1px solid #ddd;margin-top:8px;">
+  <div
+    style="display: flex; justify-content: space-between; align-items: center;border: 1px solid #ddd;margin-top:8px;">
     <table style=" padding: 10px;">
-      <tr>
-        <td >
+      <tbody>
+
+        <tr>
+          <td>
             <span style="font-size: 14px; font-weight: bold; margin-right: 8px;">Fragment Icons:</span>
             <Checkbox :label="true" size="small" v-model="loriAttr.b">B</Checkbox>
             <Checkbox :label="true" size="small" v-model="loriAttr.y">Y</Checkbox>
 
-        </td>
-        <td style="padding-left: 32px;">
-            <span style="font-size: 14px; font-weight: bold; margin-right:  8px;">  Neutral Loss: </span>
+          </td>
+          <td style="padding-left: 32px;">
+            <span style="font-size: 14px; font-weight: bold; margin-right:  8px;"> Neutral Loss: </span>
             <Checkbox :label="loriAttr.neutral.nh3" v-model="loriAttr.neutral.nh3">NH3 (*)</Checkbox>
             <Checkbox :label="loriAttr.neutral.h2o" v-model="loriAttr.neutral.h2o">H2O (o)</Checkbox>
-        </td>
-        <td style="padding-left: 32px; display: flex; align-items: center;">
+          </td>
+          <td style="padding-left: 32px; display: flex; align-items: center;">
             <div style=" display: flex; justify-content: start; align-items: center; margin-right: 8px; ">
               <div style="font-size: 14px; font-weight: bold;">Mass Tol:</div>
               <Input v-model="loriAttr.mass.tol" placeholder="" type="number" size="small"
@@ -23,13 +26,16 @@
               <Radio label="Da"></Radio>
               <Radio label="ppm"></Radio>
             </RadioGroup>
-        </td>
-         <td style="padding-left: 32px;">
+
+          </td>
+          <td style="padding-left: 32px;">
             <span style="font-size: 14px; font-weight: bold; margin-right:  8px;"> Max Ion Charge: </span>
             <Input v-model="loriAttr.max_ion_charge" @input="handleInput" placeholder="" type="number" size="small"
               style="max-width: 60px; margin-left: 4px"></Input>
-         </td>
-      </tr>
+          </td>
+
+        </tr>
+      </tbody>
     </table>
     <div style="padding-left: 64px; display: flex; justify-content: center; align-items: center; ">
       <div style="margin-top: 24x;margin-right: 8px;">
@@ -43,7 +49,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted, nextTick, watch } from "vue";
-import { loriAttr, loadSpectrumAnnotation1, loadSpectrumAnnotation2 } from "@/store";
+import { loriAttr, loadDataUp, loadDataDown } from "@/store";
 import { Spin } from 'view-ui-plus'
 
 const onUpdate = async () => {
@@ -57,8 +63,8 @@ const onUpdate = async () => {
 const onAnnotation = async () => {
   try {
     Spin.show();
-    await loadSpectrumAnnotation1();
-    await loadSpectrumAnnotation2();
+    await loadDataUp();
+    await loadDataDown();
   } finally {
     Spin.hide();
   }
@@ -75,10 +81,8 @@ const handleInput = (event: Event) => {
     loriAttr.max_ion_charge = numValue;
   }
 };
-// watch(() => loriAttr, () => {
-//   console.log(loriAttr);
-//   loadSpectrumAnnotation1();
-// }, { deep: true });
+
+
 </script>
 
 <style></style>
